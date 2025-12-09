21a0c9617165e2a5d6565a38e9fc6e2296edfde9
Dominic Vavryn <dvavryn@c3r8p5.42vienna.com>
2025-12-09 13:04:56 +0100

    changes

--- inc/cubed.h
+++ inc/cubed.h
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 18:09:34 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/11/11 17:49:53 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/12/02 16:21:11 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,30 +27,30 @@ struct s_config
 {
 	char	**raw_config;
 
-	char	*north_texture;
-	char	*east_texture;
-	char	*south_texture;
-	char	*west_texture;
+	char	*north_texture;	//
+	char	*east_texture;	//
+	char	*south_texture;	//
+	char	*west_texture;	//
 	char	*floor_color;
-	int		floor_color_hex;
+	int		floor_color_hex;//
 	char	*ceiling_color;
-	int		ceiling_color_hex;
-	int		player_position_x;
-	int		player_position_y;
-	char	player_orientation;
+	int		ceiling_color_hex;//
+	int		player_position_x;//
+	int		player_position_y;//
+	char	player_orientation;//
 };
 
 struct s_map
 {
-	int		**map;
-	size_t	map_x;
-	size_t	map_y;
+	int		**map;//
+	size_t	map_x;//
+	size_t	map_y;//
 };
 
 struct s_data
 {
-	t_config	config;
-	t_map		map;
+	t_config	config;//
+	t_map		map;//
 };
 
 void	free_data(t_data *data);
