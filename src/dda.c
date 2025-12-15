62705a41c3ca87af3c57ae357071f51a01de8758
Dominic Vavryn <dvavryn@c3r8p7.42vienna.com>
2025-12-15 21:09:10 +0100

    norm

--- src/dda.c
+++ src/dda.c
@@ -3,25 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   dda.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 11:53:02 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/12/15 19:30:35 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/12/15 21:08:40 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	cast_ray_dda_sub_one(t_data *cub, t_dda *dda)
+{
+	if (dda->raydirx < 0)
+	{
+		dda->stepx = -1;
+		dda->sidedistx = (cub->player.x / TILE - dda->mapx) * dda->deltadistx;
+	}
+	else
+	{
+		dda->stepx = 1;
+		dda->sidedistx = (dda->mapx + 1 - cub->player.x / TILE)
+			* dda->deltadistx;
+	}
+	if (dda->raydiry < 0)
+	{
+		dda->stepy = -1;
+		dda->sidedisty = (cub->player.y / TILE - dda->mapy) * dda->deltadisty;
+	}
+	else
+	{
+		dda->stepy = 1;
+		dda->sidedisty = (dda->mapy + 1 - cub->player.y / TILE)
+			* dda->deltadisty;
+	}
+}
+
+static void	cast_ray_dda_sub_two(t_data *cub, t_dda *dda)
+{
+	while (!dda->hit)
+	{
+		if (dda->sidedistx < dda->sidedisty)
+		{
+			dda->sidedistx += dda->deltadistx;
+			dda->mapx += dda->stepx;
+			dda->side = 0;
+		}
+		else
+		{
+			dda->sidedisty += dda->deltadisty;
+			dda->mapy += dda->stepy;
+			dda->side = 1;
+		}
+		if (dda->mapy < 0 || dda->mapx < 0 || dda->mapy >= (int)cub->map.map_y
+			|| dda->mapx >= (int)cub->map.map_x)
+			break ;
+		if (cub->map.map[dda->mapy][dda->mapx] == 1)
+			dda->hit = 1;
+	}
+	if (dda->side == 0)
+		dda->dist = dda->sidedistx - dda->deltadistx;
+	else
+		dda->dist = dda->sidedisty - dda->deltadisty;
+	dda->dist = dda->dist * TILE;
+}
+
 double	cast_ray_dda(t_data *cub, double angle, double *hitx, double *hity)
 {
 	t_dda	dda;
-	int		mapx;
-	int		mapy;
 
 	dda.raydirx = cos(angle);
 	dda.raydiry = sin(angle);
-	mapy = (int)(cub->player.y / TILE);
-	mapx = (int)(cub->player.x / TILE);
+	dda.mapy = (int)(cub->player.y / TILE);
+	dda.mapx = (int)(cub->player.x / TILE);
 	if (dda.raydirx == 0)
 		dda.deltadistx = 1e30;
 	else
@@ -31,50 +84,8 @@ double	cast_ray_dda(t_data *cub, double angle, double *hitx, double *hity)
 	else
 		dda.deltadisty = fabs(1 / dda.raydiry);
 	dda.hit = 0;
-	if (dda.raydirx < 0)
-	{
-		dda.stepx = -1;
-		dda.sidedistx = (cub->player.x / TILE - mapx) * dda.deltadistx;
-	}
-	else
-	{
-		dda.stepx = 1;
-		dda.sidedistx = (mapx + 1 - cub->player.x / TILE) * dda.deltadistx;
-	}
-	if (dda.raydiry < 0)
-	{
-		dda.stepy = -1;
-		dda.sidedisty = (cub->player.y / TILE - mapy) * dda.deltadisty;
-	}
-	else
-	{
-		dda.stepy = 1;
-		dda.sidedisty = (mapy + 1 - cub->player.y / TILE) * dda.deltadisty;
-	}
-	while (!dda.hit)
-	{
-		if (dda.sidedistx < dda.sidedisty)
-		{
-			dda.sidedistx += dda.deltadistx;
-			mapx += dda.stepx;
-			dda.side = 0;
-		}
-		else
-		{
-			dda.sidedisty += dda.deltadisty;
-			mapy += dda.stepy;
-			dda.side = 1;
-		}
-		if (mapy < 0 || mapx < 0 || mapy >= (int)cub->map.map_y || mapx >= (int)cub->map.map_x)
-			break ;
-		if (cub->map.map[mapy][mapx] == 1)
-			dda.hit = 1;
-	}
-	if (dda.side == 0)
-		dda.dist = dda.sidedistx - dda.deltadistx;
-	else
-		dda.dist = dda.sidedisty - dda.deltadisty;
-	dda.dist = dda.dist * TILE;
+	cast_ray_dda_sub_one(cub, &dda);
+	cast_ray_dda_sub_two(cub, &dda);
 	if (hitx)
 		*hitx = cub->player.x + dda.raydirx * dda.dist;
 	if (hity)
